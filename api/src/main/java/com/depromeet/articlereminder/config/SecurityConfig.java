4b0513111a6bc71ccc2156f7ac7b69d520853007
yeGenieee <cyj95428@gmail.com>
2021-05-08 21:25:19 +0900

    SecurityConfig 내 WhiteList 로 관리하도록 변경

--- api/src/main/java/com/depromeet/articlereminder/config/SecurityConfig.java
+++ api/src/main/java/com/depromeet/articlereminder/config/SecurityConfig.java
@@ -2,20 +2,29 @@ package com.depromeet.articlereminder.config;
 
 import com.depromeet.articlereminder.service.KakaoOAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final KakaoOAuth2UserService kakaoOAuth2UserService;
 
+    private static final String[] AUTH_WHITELIST = {
+            "/",
+            "/h2-console/**",
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/h2-console/**").permitAll();
+                .antMatchers(AUTH_WHITELIST).permitAll();
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http
@@ -24,4 +33,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/swagger-ui.html")
                 .failureUrl("/loginFailure");
     }
+
 }
