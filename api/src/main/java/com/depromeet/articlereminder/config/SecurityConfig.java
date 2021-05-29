abc8eeda7e3b5cac0c02e1c75f1c7bd174e01031
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-05-29 15:32:59 +0900

    ENH: Members 등록, 로그인 개발, oauth, serucirty 삭제

--- api/src/main/java/com/depromeet/articlereminder/config/SecurityConfig.java
+++ api/src/main/java/com/depromeet/articlereminder/config/SecurityConfig.java
@@ -1,37 +1,37 @@
-package com.depromeet.articlereminder.config;
-
-import com.depromeet.articlereminder.service.KakaoOAuth2UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
-
-@RequiredArgsConstructor
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final KakaoOAuth2UserService kakaoOAuth2UserService;
-
-    private static final String[] AUTH_WHITELIST = {
-            "/",
-            "/h2-console/**",
-            "/swagger-resources/**",
-            "/swagger-ui.html",
-            "/v2/api-docs",
-            "/webjars/**"
-    };
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(AUTH_WHITELIST).permitAll();
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http
-                .oauth2Login().userInfoEndpoint().userService(kakaoOAuth2UserService)
-                .and()
-                .defaultSuccessUrl("/swagger-ui.html")
-                .failureUrl("/loginFailure");
-    }
-
-}
+//package com.depromeet.articlereminder.config;
+//
+//import com.depromeet.articlereminder.service.KakaoOAuth2UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+//
+//@RequiredArgsConstructor
+////@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    private final KakaoOAuth2UserService kakaoOAuth2UserService;
+//
+////    private static final String[] AUTH_WHITELIST = {
+////            "/",
+////            "/h2-console/**",
+////            "/swagger-resources/**",
+////            "/swagger-ui.html",
+////            "/v2/api-docs",
+////            "/webjars/**"
+////    };
+////    @Override
+////    protected void configure(HttpSecurity http) throws Exception {
+////        http.authorizeRequests()
+////                .antMatchers(AUTH_WHITELIST).permitAll();
+////        http.csrf().disable();
+////        http.headers().frameOptions().disable();
+////        http
+////                .oauth2Login().userInfoEndpoint().userService(kakaoOAuth2UserService)
+////                .and()
+////                .defaultSuccessUrl("/swagger-ui.html")
+////                .failureUrl("/loginFailure");
+////    }
+//
+//}
--- api/src/main/java/com/depromeet/articlereminder/controller/MemberController.java
+++ api/src/main/java/com/depromeet/articlereminder/controller/MemberController.java
@@ -35,7 +35,7 @@ public class MemberController {
 
     @ApiOperation("회원 가입")
     @PostMapping("register")
-    public BaseResponse<MemberInfoResponse> register(@RequestBody MemberRegisterDTO userDto) {
+    public ResponseEntity<MemberInfoResponse> register(@RequestBody MemberRegisterDTO userDto) {
         Member user = new Member();
         user.setName(userDto.getName());
         user.setEmail(userDto.getEmail());
@@ -45,12 +45,12 @@ public class MemberController {
         long userId = memberService.join(user);
         memberService.update(userId, userAssembler.toLoginResponse(user).getToken());
 
-        return BaseResponse.of("201", "회원가입에 성공하셨습니다.", userAssembler.toUserResponse(user));
+        return ResponseEntity.ok(userAssembler.toUserResponse(user));
     }
 
     @ApiOperation("로그인을 하여 토큰을 발급받습니다.")
     @PostMapping("login")
-    public BaseResponse<LoginResponse> login(@RequestBody MemberDTO userDto) {
+    public ResponseEntity<LoginResponse> login(@RequestBody MemberDTO userDto) {
 
         boolean isValid = true;
         String token = userDto.getToken();
@@ -71,128 +71,6 @@ public class MemberController {
         if (!isValid) {
             throw new InvalidAccessTokenException();
         }
-        return BaseResponse.of("202", "로그인에 성공하셨습니다.", userAssembler.toLoginResponse(member));
+        return ResponseEntity.ok(userAssembler.toLoginResponse(member));
     }
-
-    //@LoginCheck(type = LoginCheck.UserType.USER)
-    @GetMapping("info")
-    public ResponseEntity<Member> memberInfo(@RequestParam(required = false) long userId) {
-        Member memberInfo = memberService.findOne(userId);
-        return new ResponseEntity<Member>(memberInfo, HttpStatus.OK);
-    }
-
-    //@LoginCheck(type = LoginCheck.UserType.USER)
-    @ApiOperation("사용자의 정보를(마이페이지를) 조회합니다. - 사용자 id 필요, 인증이 필요한 요청입니다.")
-    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, paramType = "header")
-    @GetMapping("mypage/info")
-    public BaseResponse<UserMyPageResponse> userPageInfo(@RequestParam(required = false) String email) {
-        BadgeResponse badge = BadgeResponse.builder()
-                .badgeId(1L)
-                .badgeName("0~5000 포인트 뱃지")
-                .badgeURL("https://s3-alpha-sig.figma.com/img/14e9/9e13/1932c8a44e20ccbc191fb7d9976b8dc9?Expires=1621209600&Signature=OgaQ2TVqUg8niOuCrSYD8DX7LxzEv2jGvGgeUQNg~CSdE0mq9yz~6b788lC2dO1zFj5R~fXnnfIBsA6FX4x9vzcnIqOrmtVCthwXyJ5AmZBLWedvH9cra5bqF~Up36-Tbym7TVKHvFzATtgCOKX4xzsCD234EGD~y5r9bHiEdS8Z3cINvEt5MW2mZx19-Gi4qXe~l0G-wRNjZdGkUd5ygMSAi8ZwGsad2UuYW8IPAUVAF0aZD19ElsZAWKuarxzLehUiN7MWpKFGTAKoPXl2kybOfw2n3H9MtNqzEcg0GBGT60l6WpZ7LKU7aVpCtip-itDcFkA9ZHc1hysvO~-mdg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")
-                .category(BadgeCategory.POINT)
-                .conditions("포인트가 5000점 이하인 경우 획득")
-                .createdAt(LocalDateTime.now())
-                .userId(1L)
-                .build();
-
-        UserMyPageResponse myPageResponse = UserMyPageResponse.builder()
-                .userId(1L)
-                .nickName("칠성파")
-                .totalPoint(4235L)
-                .seasonCount(53L)
-                .createdAt(LocalDateTime.now().minusDays(60L))
-                .totalReadCount(52L)
-                .badge(badge)
-                .build();
-
-        return BaseResponse.of("202", "사용자 정보 조회에 성공했습니다.", myPageResponse);
-    }
-
-    //@LoginCheck(type = LoginCheck.UserType.USER)
-    @ApiOperation("사용자의 뱃지 히스토리를 조회합니다. - 사용자 id 필요, 인증이 필요한 요청입니다.")
-    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, paramType = "header")
-    @GetMapping("mypage/badges")
-    public BaseResponse<Page<BadgeResponse>> userBadges(@RequestParam(required = false) String email,
-                                                        @RequestParam(required = false, defaultValue = "0") int pageNumber,
-                                                        @RequestParam(required = false, defaultValue = "12") int pageSize) {
-        BadgeResponse badge1 = BadgeResponse.builder()
-                .badgeId(1L)
-                .userId(1L)
-                .badgeName("0~5000 포인트 뱃지")
-                .badgeURL("https://s3-alpha-sig.figma.com/img/14e9/9e13/1932c8a44e20ccbc191fb7d9976b8dc9?Expires=1621209600&Signature=OgaQ2TVqUg8niOuCrSYD8DX7LxzEv2jGvGgeUQNg~CSdE0mq9yz~6b788lC2dO1zFj5R~fXnnfIBsA6FX4x9vzcnIqOrmtVCthwXyJ5AmZBLWedvH9cra5bqF~Up36-Tbym7TVKHvFzATtgCOKX4xzsCD234EGD~y5r9bHiEdS8Z3cINvEt5MW2mZx19-Gi4qXe~l0G-wRNjZdGkUd5ygMSAi8ZwGsad2UuYW8IPAUVAF0aZD19ElsZAWKuarxzLehUiN7MWpKFGTAKoPXl2kybOfw2n3H9MtNqzEcg0GBGT60l6WpZ7LKU7aVpCtip-itDcFkA9ZHc1hysvO~-mdg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")
-                .category(BadgeCategory.POINT)
-                .conditions("포인트가 5000점 이하인 경우 획득")
-                .createdAt(LocalDateTime.now())
-                .userId(1L)
-                .build();
-
-        BadgeResponse badge2 = BadgeResponse.builder()
-                .badgeId(2L)
-                .userId(1L)
-                .badgeName("5001~10000 포인트 뱃지")
-                .badgeURL("https://s3-alpha-sig.figma.com/img/14e9/9e13/1932c8a44e20ccbc191fb7d9976b8dc9?Expires=1621209600&Signature=OgaQ2TVqUg8niOuCrSYD8DX7LxzEv2jGvGgeUQNg~CSdE0mq9yz~6b788lC2dO1zFj5R~fXnnfIBsA6FX4x9vzcnIqOrmtVCthwXyJ5AmZBLWedvH9cra5bqF~Up36-Tbym7TVKHvFzATtgCOKX4xzsCD234EGD~y5r9bHiEdS8Z3cINvEt5MW2mZx19-Gi4qXe~l0G-wRNjZdGkUd5ygMSAi8ZwGsad2UuYW8IPAUVAF0aZD19ElsZAWKuarxzLehUiN7MWpKFGTAKoPXl2kybOfw2n3H9MtNqzEcg0GBGT60l6WpZ7LKU7aVpCtip-itDcFkA9ZHc1hysvO~-mdg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")
-                .category(BadgeCategory.POINT)
-                .conditions("포인트가 5001~10000점인 경우 획득")
-                .createdAt(LocalDateTime.now())
-                .userId(1L)
-                .build();
-
-        BadgeResponse badge3 = BadgeResponse.builder()
-                .badgeId(3L)
-                .userId(1L)
-                .badgeName("0~500 포인트 뱃지")
-                .badgeURL("https://s3-alpha-sig.figma.com/img/14e9/9e13/1932c8a44e20ccbc191fb7d9976b8dc9?Expires=1621209600&Signature=OgaQ2TVqUg8niOuCrSYD8DX7LxzEv2jGvGgeUQNg~CSdE0mq9yz~6b788lC2dO1zFj5R~fXnnfIBsA6FX4x9vzcnIqOrmtVCthwXyJ5AmZBLWedvH9cra5bqF~Up36-Tbym7TVKHvFzATtgCOKX4xzsCD234EGD~y5r9bHiEdS8Z3cINvEt5MW2mZx19-Gi4qXe~l0G-wRNjZdGkUd5ygMSAi8ZwGsad2UuYW8IPAUVAF0aZD19ElsZAWKuarxzLehUiN7MWpKFGTAKoPXl2kybOfw2n3H9MtNqzEcg0GBGT60l6WpZ7LKU7aVpCtip-itDcFkA9ZHc1hysvO~-mdg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")
-                .category(BadgeCategory.SEASON)
-                .conditions("2021년 4월 시즌 뱃지 획득")
-                .createdAt(LocalDateTime.now())
-                .userId(1L)
-                .build();
-
-        List<BadgeResponse> badges = Stream.of(badge1, badge2, badge3).collect(Collectors.toList());
-        Page<BadgeResponse> page = new PageImpl<>(badges);
-
-        return BaseResponse.of("202", "사용자 뱃지 리스트 조회에 성공했습니다.", page);
-    }
-
-    @ApiOperation("사용자의 푸시 알람 활성화 / 비활성화 여부를 등록합니다. - 인증이 필요한 요청입니다.")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, paramType = "header"),
-            @ApiImplicitParam(name = "userId", value = "userId", required = true, paramType = "header")
-    })
-    @PostMapping("mypage/alarm")
-    public BaseResponse<UserMyPageResponse> postPushAlarmStatus(@RequestHeader(name = "Authorization") String authorization,
-                                                                @RequestHeader(name = "userId") Long userId,
-
-                                                                @ApiParam(name = "alarmEnabled",
-                                                                        type = "string",
-                                                                        example = "T",
-                                                                        value = "푸시 알람 활성화 여부 (활성화 : T, 비활성화 : F)",
-                                                                        required = true)
-                                                                @RequestParam(required = true) String alarmEnabled) {
-
-        BadgeResponse badge = BadgeResponse.builder()
-                .badgeId(1L)
-                .badgeName("0~5000 포인트 뱃지")
-                .badgeURL("https://s3-alpha-sig.figma.com/img/14e9/9e13/1932c8a44e20ccbc191fb7d9976b8dc9?Expires=1621209600&Signature=OgaQ2TVqUg8niOuCrSYD8DX7LxzEv2jGvGgeUQNg~CSdE0mq9yz~6b788lC2dO1zFj5R~fXnnfIBsA6FX4x9vzcnIqOrmtVCthwXyJ5AmZBLWedvH9cra5bqF~Up36-Tbym7TVKHvFzATtgCOKX4xzsCD234EGD~y5r9bHiEdS8Z3cINvEt5MW2mZx19-Gi4qXe~l0G-wRNjZdGkUd5ygMSAi8ZwGsad2UuYW8IPAUVAF0aZD19ElsZAWKuarxzLehUiN7MWpKFGTAKoPXl2kybOfw2n3H9MtNqzEcg0GBGT60l6WpZ7LKU7aVpCtip-itDcFkA9ZHc1hysvO~-mdg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")
-                .category(BadgeCategory.POINT)
-                .conditions("포인트가 5000점 이하인 경우 획득")
-                .createdAt(LocalDateTime.now())
-                .userId(1L)
-                .build();
-
-        UserMyPageResponse myPageResponse = UserMyPageResponse.builder()
-                .userId(1L)
-                .nickName("칠성파")
-                .totalPoint(4235L)
-                .seasonCount(53L)
-                .createdAt(LocalDateTime.now().minusDays(60L))
-                .totalReadCount(52L)
-                .alarmEnabled("T".equals(alarmEnabled))
-                .badge(badge)
-                .build();
-
-        return BaseResponse.of("201", "푸시 알람 활성화 여부 등록에 성공했습니다.", myPageResponse);
-    }
-
 }
