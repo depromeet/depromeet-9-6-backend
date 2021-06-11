96a4629409bff29f073628f3e71cc35a00ce2ee8
yeGenieee <cyj95428@gmail.com>
2021-06-12 00:34:57 +0900

    chore: 로깅 설정

--- api/src/main/java/com/depromeet/articlereminder/ArticleReminderApplication.java
+++ api/src/main/java/com/depromeet/articlereminder/ArticleReminderApplication.java
@@ -1,5 +1,6 @@
 package com.depromeet.articlereminder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,10 +12,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@Slf4j
 public class ArticleReminderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArticleReminderApplication.class, args);
+
+		log.error("error");
+		log.warn("warn");
+		log.info("info");
+		log.trace("trace");
+		log.debug("debug");
 	}
 
 }
