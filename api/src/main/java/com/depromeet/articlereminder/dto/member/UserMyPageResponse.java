700c6b8f4af6864a61af5ba560b5289edc4c20bb
yeGenieee <cyj95428@gmail.com>
2021-06-12 00:45:55 +0900

    fix: Builder 제거

--- api/src/main/java/com/depromeet/articlereminder/dto/member/UserMyPageResponse.java
+++ api/src/main/java/com/depromeet/articlereminder/dto/member/UserMyPageResponse.java
@@ -5,9 +5,7 @@ import com.depromeet.articlereminder.dto.badge.BadgeResponse;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
