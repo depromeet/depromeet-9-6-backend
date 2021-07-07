4bbf634762e145d345177d36a4db5cfee1ed20a1
yeGenieee <cyj95428@gmail.com>
2021-07-08 02:55:33 +0900

    feat: 서버로 들어오는 모든 request도 로깅

---
+++ api/src/main/java/com/depromeet/articlereminder/config/AccessLogConfig.java
@@ -0,0 +1,26 @@
+package com.depromeet.articlereminder.config;
+
+import org.apache.catalina.valves.AccessLogValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class AccessLogConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setIncludeClientInfo(true);
+        filter.setAfterMessagePrefix(" [REQUEST] : ");
+
+        return filter;
+    }
+}
